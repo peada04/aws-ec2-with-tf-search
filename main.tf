a8819da3115438031d226b0ae9b69e3be99618cc
peada04 <dpeacock@hashicorp.com>
2021-02-03 08:34:06 -0500

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.medium"
+  count         = 1
+
+  tags = {
+    Name = "workshop_demo_2021"
+    name = "${var.prefix}-vpc-${var.region}"
+    owner = var.prefix
+    region = var.hashi-region
+    purpose = var.purpose
+    ttl = var.ttl
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,47 @@
+variable "cidr_vpc" {
+  description = "CIDR block for the VPC"
+  default     = "10.1.0.0/16"
+}
+variable "cidr_subnet" {
+  description = "CIDR block for the subnet"
+  default     = "10.1.0.0/24"
+}
+
+variable "environment_tag" {
+  description = "Environment tag"
+  default     = "Learn"
+}
+
+variable "prefix" {
+  description = "used to associate resources with a person"
+  default = "dpeacock"
+}
+
+variable "region"{
+  description = "The region Terraform deploys your instance"
+}
+
+#variable "ami_id_value" {
+ # description = "Value to use for the ami_id"
+#}
+
+variable "owner" {
+  default = "dpeacock"
+  description = "person from HC that deployed the resource"
+}
+
+variable "hashi-region" {
+  default = "nymetro"
+  description = "HC region that the owner belongs to"
+}
+
+variable "purpose" {
+  default = "testing"
+  description = "what the resource is for"
+} 
+
+variable "ttl" {
+  default = "12"
+  description = "time to live before reaper should delete"
+}
+
