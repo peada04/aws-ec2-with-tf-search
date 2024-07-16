5d58e631dadba834a305b2d061a84bb622ead2e6
peada04 <dpeacock@hashicorp.com>
2024-07-16 08:19:21 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,11 +34,11 @@ resource "aws_instance" "web" {
 }
 
 check "aws_instances_stopped" {
-  data "aws_instances" "web" {
+  data "aws_instance" "web" {
     instance_state_names = ["stopped"]
   }
   assert {
-    condition     = length(data.aws_instances.web) > 0
+    condition     = length(data.aws_instance.web) > 0
     error_message = format("Found Instances have stopped! Instance ID’s: %s", data.aws_instance.web.ids)
   }
 }
