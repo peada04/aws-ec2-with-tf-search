049e57be32b21e24bdfc7d051c2b09475424ba30
peada04 <dpeacock@hashicorp.com>
2025-05-05 11:25:56 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.small"
+  instance_type = "t2.micro"
   count         = 1
   tags = {
     name = "${var.prefix}-vpc-${var.region}"
