8f8bed8fd4aed4545a35563745873b79ec397661
peada04 <dpeacock@hashicorp.com>
2025-10-01 12:34:36 -0400

    Update main.tf
    removed list block since that needs to be in a specific file

--- main.tf
+++ main.tf
@@ -18,10 +18,6 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-list "aws_instance" "search_example" {
-  provider = aws
-}
-
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
